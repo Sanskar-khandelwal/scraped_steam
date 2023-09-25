c4011750fc83c50f681781df75a8cac3f24ee84f
Sanskar Khandelwal <Sanskarkhandelwal336@gmail.com>
2023-09-25 14:02:54 +0530

    made a simple webpage using the api

--- web/app.py
+++ web/app.py
@@ -1,1 +1,15 @@
-from flask import flask
+from flask import Flask, render_template
+import requests
+import json
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    resp = requests.get(
+        "http://127.0.0.1:9080/crawl.json?start_requests=true&spider_name=best_selling").json()
+    return render_template('index.html', games=resp.get('items'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
